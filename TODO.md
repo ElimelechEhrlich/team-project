673648fddcb8c98b2e79d59335429662920d0163
Elimelech Ehrlich <ee525566@gmail.com>
2025-11-10 10:08:16 +0200

    create library class with attribute and add_book method and add_user method

--- TODO.md
+++ TODO.md
@@ -14,6 +14,7 @@ User:
         name
         id
         borrowed_books:list[books]
+iu:
         
 Library:
     attributes:
--- classes/library.py
+++ classes/library.py
@@ -1,8 +1,15 @@
-from 
+from class_Book import Book
+from user import User
+
 class Library:
     def __init__(self):
         self.list_of_books = []
         self.list_of_users = []
 
     def add_book(self):
-        self.list_of_books.append(Book)
+        self.list_of_books.append(Book())
+
+    def add_user(self):
+        self.list_of_users.append(User())
+
+    
