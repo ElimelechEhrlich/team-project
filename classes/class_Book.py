9815b766e8fca461c1b2f18047e1ea14871e64e7
Israel_Winnett <arulik98@gmail.com>
2025-11-10 12:43:17 +0200

    class menu with menu and user input completed

--- classes/class_Book.py
+++ classes/class_Book.py
@@ -3,8 +3,8 @@ class Book:
         self.title = title
         self.authr = auther
         self.ISBN = ISBN
-        self.is_available = True
+        self.is_availble = True
 
     def __str__(self):
-        return f"title: {self.title}, auther: {self.authr}, is_avilable:{self.is_available}"
+        return f"title: {self.title}, auther: {self.authr}, is_avilable:{self.is_availble}"
     
