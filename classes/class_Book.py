70b6dc845d6ebd89abf25f0540145fc230875f2e
Elimelech Ehrlich <ee525566@gmail.com>
2025-11-10 15:31:26 +0200

    fixed main file

--- classes/class_Book.py
+++ classes/class_Book.py
@@ -1,10 +1,10 @@
 from uuid import uuid4
 class Book:
-    def __init__(self,title: str, auther: str, ISBN:int):
+    def __init__(self,title: str, auther: str):
         self.title = title
         self.authr = auther
-        self.ISBN = uuid4()
-        self.is_available = True
+        self.ISBN:int = uuid4()
+        self.is_available:bool = True
 
     def __str__(self):
         return f"title: {self.title}, auther: {self.authr}, is_avilable:{self.is_available}"
--- main.py
+++ main.py
@@ -3,21 +3,24 @@ from classes.user import User
 from classes.library import Library
 from classes.iu import Menu
 
+library1 = Library()
+book1 = Book('A Little Book for Little Children', 'Thomas White')
+book2 = Book('A Token for Children', 'James Janeway')
+book3 = Book('Divine Songs', 'Isaac Watts')
+book4 = Book('A Description of Three Hundred Animals', 'Thomas Boreman')
+book5 = Book('The Gigantick History of the Two Famous Giants', 'Thomas Boreman')
+book6 = Book('Winter-Evening Entertainments', 'Nathaniel Crouch')
+book7 = Book('The History of Fortunatus', 'Thomas Churchyard')
+user1 = User('elimelch ehrlich')
+books = [book1,book2,book3,book4,book5,book6,book7]
+for book in books:
+    library1.add_book(book)
 
-if __name__ == '__nain__':
-    library1 = Library()
-    book1 = Book('A Little Book for Little Children', 'Thomas White')
-    book2 = Book('A Token for Children', 'James Janeway')
-    book3 = Book('Divine Songs', 'Isaac Watts')
-    book4 = Book('A Description of Three Hundred Animals', 'Thomas Boreman')
-    book5 = Book('The Gigantick History of the Two Famous Giants', 'Thomas Boreman')
-    book6 = Book('Winter-Evening Entertainments', 'Nathaniel Crouch')
-    book7 = Book('The History of Fortunatus', 'Thomas Churchyard')
-    user1 = User('elimelch ehrlich')
-    books = [book1,book2,book3,book4,book5,book6,book7]
-    for book in books:
-        library1.add_book(book)
+if __name__=='__main__':
+
+
     library1.add_user(user1)
-
+    print(book1.title)
+    print(library1.list_of_books)
     print(library1.list_availble_books())
 
